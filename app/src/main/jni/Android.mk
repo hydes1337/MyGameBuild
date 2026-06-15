bcc4a67a958c7ddbda03f0eefb0969f4f0175c32
hydes1337 <hydes1337@gmail.com>
2026-06-15 19:19:30 +0300

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -18,6 +18,7 @@ LOCAL_MODULE    := gmm
 LOCAL_CFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -fpermissive -fexceptions
 LOCAL_CPPFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -Werror -std=c++17
 LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fpermissive -Wall -fexceptions
+LOCAL_CPPFLAGS += -frtti
 LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all,-llog
 LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 LOCAL_ARM_MODE := arm
